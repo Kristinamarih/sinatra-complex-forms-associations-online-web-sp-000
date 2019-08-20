83ab77ed8be5550bc1b6d73a0b3a60d7be34f7d8
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-08-20 17:14:03 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -10,12 +10,18 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do 
+<<<<<<< HEAD
     @pet = Pet.create(name: params[:pet][:name])
     if params[:pet][:owner_ids]
       @pet.owner_id = params[:pet][:owner_ids].first
       elsif params[:owner_name]
       owner = Owner.create(name: params[:owner_name])
       @pet.owner = owner
+=======
+    @pet = Pet.create(params["pet"])
+    if !params["owner"]["name"].empty?
+      @pet.owner << Owner.create(name: params["owner"]["name"])
+>>>>>>> c0c46f18d2559fbda1a4e5b136b5f5aabeaa870c
     end
     @pet.save
     redirect to "pets/#{@pet.id}"
@@ -31,6 +37,7 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
+<<<<<<< HEAD
   post '/pets/:id' do 
     @pet = Pet.find(params[:id])
     @pet.update(name: params[:pet][:name])
@@ -45,6 +52,15 @@ class PetsController < ApplicationController
     end
     @pet.save
 
+=======
+  patch '/pets/:id' do 
+    @pet = Pet.find(params[:id])
+    @pet.update(params["pet"])
+    if !params["owner"]["name"].empty?
+      @pet.owner << Owner.create(name: params["owner"]["name"])
+    end
+    @pet.save
+>>>>>>> c0c46f18d2559fbda1a4e5b136b5f5aabeaa870c
     redirect to "pets/#{@pet.id}"
   end
 end
